0da0fad3f149bfa5ff23d555d385c82c67464f52
Blake250 <ozoekwecelestine@gmail.com>
2024-05-15 01:44:38 +0100

    i made some changes in the userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -40,7 +40,7 @@ const registerUser = asyncHandler( async (req, res)=>{
     const {_id, name, email,role} = user
 
     res.cookie("token", token, {
-      path: "/",
+      path: "/home",
       httpOnly: true,
       expires: new Date(Date.now() + 1000 * 86400 ),
     //  secure:true,
@@ -84,7 +84,7 @@ const LoginUser = asyncHandler((async(req, res)=>{
   if(user && passwordIsCorrect){
     const newUser = await User.findOne({email : email}).select("-password")
     res.cookie(  "token", token, {
-      path:"/",
+      path:"/home",
       httpOnly:true,
       expires: new Date(Date.now()  + 1000 * 86400),
      // secure:true,
@@ -105,7 +105,7 @@ const LoginUser = asyncHandler((async(req, res)=>{
 // LogOut User
 const logOutUser = asyncHandler(async(req, res)=>{
   res.cookie("token", "", {
-    path:"/",
+    path:"/home",
    httpOnly:true,
     expires: new Date(0),
        // secure:true,
