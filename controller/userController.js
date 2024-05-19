07fd3ec8030ee1ac516e24d47140076ab21ef5c4
Christian <89995961+Blake250@users.noreply.github.com>
2024-05-19 23:05:09 +0100

    Update userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -42,7 +42,7 @@ const registerUser = asyncHandler( async (req, res)=>{
     const {_id, name, email,role} = user
 
     res.cookie("token", token, {
-      path: "/home",
+      path: "/",
       httpOnly: true,
       expires: new Date(Date.now() + 1000 * 86400 ),
      secure:true,
@@ -87,7 +87,7 @@ const LoginUser = asyncHandler((async(req, res)=>{
   if(user && passwordIsCorrect){
     const newUser = await User.findOne({email : email}).select("-password")
     res.cookie(  "token", token, {
-      path:"/home",
+      path:"/",
       httpOnly:true,
       expires: new Date(Date.now()  + 1000 * 86400),
   secure:true,
@@ -109,7 +109,7 @@ const LoginUser = asyncHandler((async(req, res)=>{
 // LogOut User
 const logOutUser = asyncHandler(async(req, res)=>{
   res.cookie("token", "", {
-    path:"/home",
+    path:"/",
    httpOnly:true,
     expires: new Date(0),
  secure:true,
