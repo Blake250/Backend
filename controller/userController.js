431f2e2487d947b06845e2b3d8848f41ed83c2eb
Blake250 <ozoekwecelestine@gmail.com>
2024-08-31 13:42:55 +0100

    still on it

--- controller/userController.js
+++ controller/userController.js
@@ -33,7 +33,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   // Check if user already exists
-  const userExists = await User.findOne({ email: req.body.email });
+  const userExists = await User.findOne({ email: req.user.email });
   if (userExists) {
     res.status(400);
     throw new Error("User already exists");
