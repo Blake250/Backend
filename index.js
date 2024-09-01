ae75eff94c0224c3f8788c8f4de8e4f17134f38c
Blake250 <ozoekwecelestine@gmail.com>
2024-09-01 03:49:23 +0100

    making this changes to index.js

---
+++ index.js
@@ -0,0 +1,143 @@
+
+
+
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+//const path = require("path");
+const errorHandler = require("./middleware/errorMiddleWare.js");
+const userRouter = require("./routes/userRoute.js");
+const productRoute = require("./routes/productRoute.js");
+const categoryRoute = require("./routes/categoryRoute.js");
+const brandRoute = require("./routes/brandRoutes.js");
+const couponRoute = require("./routes/couponRoute.js");
+const orderRoute = require("./routes/orderRoute.js");
+const transactionRoute = require("./routes/transactionRoute.js");
+//const bodyParser = require('body-parser')
+dotenv.config();
+const app = express();
+
+
+// app.use(express.static(path.join(__dirname, 'frontend_ecommerce/build' )))
+
+
+
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+    
+      "https://shopito-app-zs1v.onrender.com",
+    'https://api-shopito-app.onrender.com',
+    'http://localhost:3000',
+    'http://localhost:8000'
+   
+  
+      
+      
+
+    ];
+    
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.log("Blocked origin:", origin);
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+   credentials: true,
+  allowedHeaders: ["Content-Type", 'Cookie',"Authorization", 'X-Requested-With', 'Accept',  ],
+  allowedMethods: ['GET', 'POST', 'PATCH', 'DELETE',  'OPTIONS', 'PUT']
+};
+
+
+
+
+
+// var whitelist = ['http://localhost:8000', 'http://localhost:3000']; //white list consumers
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(null, false);
+//     }
+//   },
+//   methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
+//   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+// };
+
+app.use(cors(corsOptions));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+
+
+
+
+//app.use(cors(corsOptions));
+mongoose.connect(process.env.MONGODB_URL, {
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true
+}).then(() => console.log('Connected to the database!'))
+  .catch(err => console.error('Connection error:', err));
+
+  
+  app.use("/api/transaction", transactionRoute);
+app.use(express.json());
+
+
+app.use("/api/user", userRouter);
+app.use("/api/products", productRoute);
+app.use("/api/category", categoryRoute);
+app.use("/api/brand", brandRoute);
+app.use("/api/coupon", couponRoute);
+app.use("/api/order", orderRoute);
+//app.use("/api/transaction", transactionRoute);
+
+
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'frontend_ecommerce/build', 'index.html'));
+// });
+
+
+
+
+
+app.use(errorHandler);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
