6f3913337a90f481d164a1000f58002e434747d5
Blake250 <ozoekwecelestine@gmail.com>
2024-09-04 15:42:00 +0100

    fixing a server rror

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ mongoose.connect(process.env.MONGODB_URL)
 
  
  //Serve static files from the React frontend app
- const buildPath = path.join(__dirname, '..', '/frontend_ecommerce', 'build');
+ const buildPath = path.join(__dirname, '../frontend_ecommerce', 'build');
 //  const buildPath = path.join(__dirname, '/frontend_ecommerce', 'build');
  console.log("Serving static files from:", buildPath);
  
